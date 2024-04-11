c1d0f56a148a32c3f22f459c794cf997f9087fbb
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-04-11 17:03:13 +0300

    Создал первый router. Вывел фразу Hello world

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from app.config import TOKEN
 from app.data_base.models import async_main
+from app.handlers import router
 
 
 async def main():
     await async_main()
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 
