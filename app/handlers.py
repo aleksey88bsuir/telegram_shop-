3b87cd10e0d54353006a29835e91c7ae6725e86a
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-04-11 18:13:18 +0300

    Добавил функцию действия на кнопку 'Каталог'

--- app/handlers.py
+++ app/handlers.py
@@ -2,10 +2,27 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 
+import app.keyboards as kw
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def app_start(message: Message):
-    await message.answer("Hello world!")
+    await message.answer("Добро пожаловать в интернет-магазин!",
+                         reply_markup=kw.main)
+
+
+@router.message(F.text == "Каталог")
+async def catalog(message: Message):
+    await message.answer('Выберите категорию')
+
+#
+# @router.message(F.text == "Корзина")
+# async def basket(message: Message):
+#     await message.answer('')
+#
+#
+# @router.message(F.text == "Контакты")
+# async def contacts(message: Message):
+#     await message.answer('')
